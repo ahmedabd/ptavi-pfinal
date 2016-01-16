f9f7f9569002a2b9ae8976e5255e8298fc792eba
ahmedabd <ahmedabdulkarim06@gmail.com>
2016-01-16 18:52:02 +0100

    Funciona "todo"

--- uaclient.py
+++ uaclient.py
@@ -69,12 +69,11 @@ if METODO == 'REGISTER':
 
     if RCV == ['SIP/2.1 401 Unauthorized']:
         print("Enviando: " + METODO)
-        nonce = datos_lista[1].split('=')[1]
+        nonce = datos_lista[1].split('=')[1].split()[0]
         m = hashlib.md5()
-        print(passwd + nonce)
-        m.update(bytes(passwd + nonce.strip(), 'utf-8'))
+        m.update(bytes(passwd + nonce, 'utf-8'))
         response = m.hexdigest()
-        my_socket.send((bytes(LINE1, 'utf-8') + bytes('Expires:' + ' ' + str(OPCION), 'utf-8') + b'\r\nAuthorization: response=' + bytes(response, 'utf-8') + b'\r\n\r\n'))
+        my_socket.send((bytes(LINE1, 'utf-8') + bytes('Expires:' + ' ' + str(OPCION), 'utf-8') + b'\r\nAuthorization: Digest response=' + bytes(response, 'utf-8') + b'\r\n\r\n'))
         data = my_socket.recv(1024)
         datos = data.decode('utf-8')
         datos_lista = datos.split('\r\n')
@@ -93,10 +92,10 @@ if METODO == 'INVITE':
     RCV = datos_lista[0:5]
 
     if datos_lista[0:1] == ['SIP/2.0 404 User not Found']:
-        pass
+        log(log_path, 'Recived from' + ip_proxy + ':' + puerto_proxy + ' ' + ' '.join(datos_lista) + '\r\n')
 
     if RCV == ['SIP/2.0 100 Trying', '', 'SIP/2.0 180 Ring', '', 'SIP/2.0 200 OK']:
-        dicc[datos_lista[8].split('=')[1].split()[0]] = [datos_lista[11].split()[1], datos_lista[8].split()[1]]
+        dicc[datos_lista[8].split()[0]] = [datos_lista[11], datos_lista[8].split()[1]]
         log(log_path, 'Sent to ' + ip_proxy + ':' + puerto_proxy + ' ' + ' '.join(LINE3.split('\r\n')) + '\r\n')
         print("Enviando: " + 'ACK')
         my_socket.send(bytes(LINE3, 'utf-8'))
@@ -104,10 +103,11 @@ if METODO == 'INVITE':
         datos = data.decode('utf-8')
         datos_lista = datos.split('\r\n')
         print('Recibido -- ', datos)
-        [puerto_receptor, ip_receptor] = dicc[LINE1.split()[1].split(':')[1]]
+        [puerto_receptor, ip_receptor] = dicc[LINE2.split()[1].split(':')[1]]
         aEjecutar = './mp32rtp -i ' + ip_receptor + ' ' '-p ' + str(puerto_receptor) + ' ' + '< ' + FICH_AUDIO
         print('Vamos a ejecutar', aEjecutar)
         os.system(aEjecutar)
+
 
 if METODO == 'BYE':
     log(log_path, 'Sent to ' + ip_proxy + ':' + puerto_proxy + ' ' + ' '.join(LINE4.split('\r\n')) + '\r\n')
